60ebb1f3a81c20e9f294aa9f59857c5e2de56f91
Gurgen Hovhannisyan <gurgenhovhannisyan@Gurgens-MacBook-Pro-2.local>
2019-10-31 20:39:51 +0400

    Initial

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/gurgenhovhannisyan/work/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gurgenhovhannisyan/work/Flutter/test_proj"
+export "FLUTTER_TARGET=/Users/gurgenhovhannisyan/work/Flutter/test_proj/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/gurgenhovhannisyan/work/Flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
